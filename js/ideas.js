f29e6e27588adbc8dbffd7b80f2febe6327c4b5a
attoher <atha2dj@gmail.com>
2026-02-14 16:52:24 +0700

    Fix: remove room numbers, show all categories in result, fix ideas display

--- js/ideas.js
+++ js/ideas.js
@@ -237,7 +237,7 @@ function renderRoomColumn(item) {
     roomInputs.length = ideaInputs.length;
     
     let html = '<div class="step-col step-col-room">';
-    html += `<div class="col-header"><span class="col-icon"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#059669" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></span> Ruangan #${item.roomNum}</div>`;
+    html += `<div class="col-header"><span class="col-icon"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#059669" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></span> Ruangan</div>`;
     
     ideaInputs.forEach((_, idx) => {
         const roomVal = roomInputs[idx] || '';
